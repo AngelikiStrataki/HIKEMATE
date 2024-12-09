50435cb3de5fc5640c2cf76fe337a399eb8c0740
AthinaApo <auhapo@gmail.com>
2024-12-09 22:20:42 +0200

    Update example.test.js
    Άνοιγμα και κλείσιμο του server πριν και μετά το test αντίστοιχα

--- nodejs-server-server-generated/tests/example.test.js
+++ nodejs-server-server-generated/tests/example.test.js
@@ -1,1 +1,22 @@
+const http = require('http');
+const test = require('ava');
+const listen = require('test-listen');
+const got = require('got');
+
+const app = require('../index');
+
+var Trail = require('../service/TrailService');
+var { trailPhotos } = require('../service/TrailService'); // Εάν το αρχείο είναι μέσα στον φάκελο service
+
+
+
+test.before(async (t) => {
+	t.context.server = http.createServer(app);
+	t.context.prefixUrl = await listen(t.context.server);
+	t.context.got = got.extend({ http2: true, throwHttpErrors: false, responseType: "json", prefixUrl: t.context.prefixUrl });
+});
+
+test.after.always((t) => {
+	t.context.server.close();
+});
 
