57fde6f2b6d6931c596245774ee219220b0c97fb
AthinaApo <auhapo@gmail.com>
2024-12-12 13:21:49 +0200

    Update example.test.js
    ενα ακόμα τεστ

--- nodejs-server-server-generated/tests/example.test.js
+++ nodejs-server-server-generated/tests/example.test.js
@@ -34,3 +34,17 @@ test('GET rate - viewTrailRating should return average rating', async (t) => {
         average_rating: 4 
     });
 });
+
+test('GET rate - viewTrailRating should return 0 if no ratings', async (t) => {
+    const trailId = 2;
+
+    // Κλήση της API για το rate του trail χωρίς αξιολογήσεις
+    const response = await t.context.got(`trail/${trailId}/rate`);
+    
+    
+    t.is(response.statusCode, 200);
+    t.deepEqual(response.body, { 
+        trail_id: trailId, 
+        average_rating: 0 
+    });
+});
