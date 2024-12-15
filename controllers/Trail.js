bf130dc42815d2594f8777cf2a9a44104929f3f7
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2024-12-15 18:16:53 +0200

    Update Trail.js

--- controllers/Trail.js
+++ controllers/Trail.js
@@ -23,26 +23,6 @@ module.exports.rateTrail = function rateTrail (req, res, next, body, trail_id) {
     });
 };
 
-module.exports.sendmessage = function sendmessage (req, res, next, body, forum_id, trail_id) {
-  Trail.sendmessage(body, forum_id, trail_id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.storeFavourite = function storeFavourite (req, res, next, body, trail_id) {
-  Trail.storeFavourite(body, trail_id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
 module.exports.uploadPhotos = function uploadPhotos (req, res, next, trail_id) {
   Trail.uploadPhotos(trail_id)
     .then(function (response) {
