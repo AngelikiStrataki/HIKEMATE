cb8288d5adabc55d962c53adf776b0c08a2ea424
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2024-12-15 17:48:44 +0200

    Update Trail.js

--- controllers/Trail.js
+++ controllers/Trail.js
@@ -63,13 +63,13 @@ module.exports.useForum = function useForum (req, res, next, trail_id) {
     });
 };
 
-module.exports.view a specific trail = function view a specific trail (req, res, next, trail_id) {
-  Trail.view a specific trail(trail_id)
+module.exports.view_a_specific_trail = function view_a_specific_trail(req, res, next, trail_id) {
+  Trail.view_a_specific_trail(trail_id)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
+    .catch(function (error) {
+      utils.writeJson(res, { message: error.message }, 404);
     });
 };
 
