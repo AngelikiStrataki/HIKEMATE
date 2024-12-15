96392abe9bf25d478a38ef1cff2daed5ebbd0179
AthinaApo <auhapo@gmail.com>
2024-12-15 18:16:37 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -3,17 +3,6 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-module.exports.creatEvent = function creatEvent (req, res, next, body) {
-  Event.creatEvent(body)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (error) {
-      // Αν υπάρχει σφάλμα, επιστρέφουμε το status 400 (Bad Request) ή το κατάλληλο status code
-      res.status(400).json({ message: error.message });
-    });
-};
-
 module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
