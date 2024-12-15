c0f0f7c67b5150f3e511c26bd3aa1ef4ea0a0bc0
AthinaApo <auhapo@gmail.com>
2024-12-15 18:41:14 +0200

    Update Searchfilters.js

--- controllers/Searchfilters.js
+++ controllers/Searchfilters.js
@@ -6,17 +6,14 @@ var Searchfilters = require('../service/SearchfiltersService');
 
 
 module.exports.enterSearchfilters = function enterSearchfilters(req, res, next, body) {
-  console.log('Received request body:', body);
 
   if (!body || Object.keys(body).length === 0) {
-    console.error('Invalid or missing request body');
     utils.writeJson(res, { message: 'Invalid or missing filters' }, 400);
     return;
   }
 
   Searchfilters.enterSearchfilters(body)
     .then(function (response) {
-      console.log('Searchfilters.enterSearchfilters success response:', response);
       utils.writeJson(res, response, 201);
     })
     .catch(function (error) {
