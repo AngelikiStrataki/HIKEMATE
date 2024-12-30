6cc65627bdcb4b2bb68e3fdcc3d58d557d8f60c6
AthinaApo <auhapo@gmail.com>
2024-12-30 19:31:38 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -1,10 +1,11 @@
+
 'use strict';
 
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-//module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
-module.exports.view_a_specific_event = function view_a_specific_event(res, event_id) {
+/**
+module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
       utils.writeJson(res, response, 200);
@@ -22,4 +23,23 @@ module.exports.view_events = function view_events (req, res, next) {
     .catch(function (response) {
       utils.writeJson(res, response);
     });
+};
+*/
+
+module.exports.view_a_specific_event = async function view_a_specific_event(req, res, next, event_id) {
+  try {
+    const response = await Event.view_a_specific_event(event_id);
+    utils.writeJson(res, response, 200);
+  } catch (error) {
+    utils.writeJson(res, { message: error.message }, 404);
+  }
+};
+
+module.exports.view_events = async function view_events(req, res, next) {
+  try {
+    const response = await Event.view_events();
+    utils.writeJson(res, response);
+  } catch (error) {
+    utils.writeJson(res, { message: error.message }, 500);
+  }
 };
