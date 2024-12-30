28bfc220f478d0be70f3ca285b0709fb8b92b0e8
AthinaApo <auhapo@gmail.com>
2024-12-30 19:38:54 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -4,8 +4,8 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-/**
-module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
+//module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
+module.exports.view_a_specific_event = function view_a_specific_event(_, res, _, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
       utils.writeJson(res, response, 200);
@@ -15,7 +15,8 @@ module.exports.view_a_specific_event = function view_a_specific_event(req, res, 
     });
 };
 
-module.exports.view_events = function view_events (req, res, next) {
+//module.exports.view_events = function view_events (req, res, next) {
+module.exports.view_events = function view_events (_, res, _) {
   Event.view_events()
     .then(function (response) {
       utils.writeJson(res, response);
@@ -23,23 +24,4 @@ module.exports.view_events = function view_events (req, res, next) {
     .catch(function (response) {
       utils.writeJson(res, response);
     });
-};
-*/
-
-module.exports.view_a_specific_event = async function view_a_specific_event(req, res, next, event_id) {
-  try {
-    const response = await Event.view_a_specific_event(event_id);
-    utils.writeJson(res, response, 200);
-  } catch (error) {
-    utils.writeJson(res, { message: error.message }, 404);
-  }
-};
-
-module.exports.view_events = async function view_events(req, res, next) {
-  try {
-    const response = await Event.view_events();
-    utils.writeJson(res, response);
-  } catch (error) {
-    utils.writeJson(res, { message: error.message }, 500);
-  }
 };
