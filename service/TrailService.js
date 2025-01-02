7b4f88b726f483678bff2aab897ce24e5ad6d085
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2025-01-02 22:59:56 +0200

    Update TrailService.js

--- service/TrailService.js
+++ service/TrailService.js
@@ -274,7 +274,7 @@ exports.viewPhotos = function(trail_id) {
 
 exports.useForum = function(trail_id) {
 //return new Promise(function(resolve,reject){
-  return new Promise(function(resolve, _) {
+  return new Promise(function(resolve,reject) {
     // Έλεγχος αν υπάρχει forum για το συγκεκριμένο trail_id
     const forumIndex = trail_id - 1; // Μετατροπή ID σε index
     const forum = forums[forumIndex];
