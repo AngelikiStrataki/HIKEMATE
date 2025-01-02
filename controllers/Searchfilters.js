bbe5706e47a27d6b31213bcaed61554e30b3e493
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2025-01-02 22:50:22 +0200

    Update Searchfilters.js

--- controllers/Searchfilters.js
+++ controllers/Searchfilters.js
@@ -5,7 +5,7 @@ var Searchfilters = require('../service/SearchfiltersService');
 
 
 //module.exports.enterSearchfilters = function enterSearchfilters(req, res, next, body) {
-module.exports.enterSearchfilters = function enterSearchfilters(_ , res, __ , body) {
+module.exports.enterSearchfilters = function enterSearchfilters(_ , res, body) {
 
   if (!body || Object.keys(body).length === 0) {
     utils.writeJson(res, { message: 'Invalid or missing filters' }, 400);
