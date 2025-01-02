e1b210a677641e87a6afee2a73172cb38fd3ba64
AthinaApo <auhapo@gmail.com>
2025-01-02 23:19:24 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -4,7 +4,7 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-module.exports.view_a_specific_event = function view_a_specific_event(_, res, event_id) {
+module.exports.view_a_specific_event = function view_a_specific_event(_, res, next, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
       utils.writeJson(res, response, 200);
@@ -14,7 +14,7 @@ module.exports.view_a_specific_event = function view_a_specific_event(_, res, ev
     });
 };
 
-module.exports.view_events = function view_events (_, res) {
+module.exports.view_events = function view_events (_, res, next) {
   Event.view_events()
     .then(function (response) {
       utils.writeJson(res, response);
