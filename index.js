6a2cc2097a27e20de75d86da5c334686af2ae261
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2025-01-05 13:43:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 // Middleware για διαχείριση σφαλμάτων
 //app.use((err, req, res, next) => {
-app.use((err, _, res, next) => {
+app.use((err, _, res, /* eslint-disable no-unused-vars */ _next) => {
     console.error(err.stack);
     res.status(err.status || 500).json({
         message: err.message || 'Internal Server Error',
