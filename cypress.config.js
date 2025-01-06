c2bc5ce515766b8aaa49e371f75b9a287e5e65cc
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2025-01-06 12:59:10 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(_, /* eslint-disable no-unused-vars */ _config) {
-  //  setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
