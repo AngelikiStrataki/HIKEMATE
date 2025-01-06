fbabbeb7be8fd6c1fcb89d5f048a0a462959c45c
cgkoutha <153185595+cgkoutha@users.noreply.github.com>
2025-01-06 20:32:17 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -3,6 +3,9 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
+/**
+ * Fetches details of a specific event by its ID.
+ */
 module.exports.view_a_specific_event = function view_a_specific_event(_, res, /* eslint-disable no-unused-vars */ _next, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
@@ -13,6 +16,9 @@ module.exports.view_a_specific_event = function view_a_specific_event(_, res, /*
     });
 };
 
+/**
+ * Fetches a list of all available events.
+ */
 module.exports.view_events = function view_events (_, res, /* eslint-disable no-unused-vars */ _next) {
   Event.view_events()
     .then(function (response) {
