b022fd64b8986bcd3b11fad0bf0e3b4cb41aa7b6
AthinaApo <auhapo@gmail.com>
2025-01-08 18:39:44 +0200

    Update Event.js
    Πήρα τις αλλαγές από το main

--- controllers/Event.js
+++ controllers/Event.js
@@ -3,7 +3,10 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-module.exports.view_a_specific_event = function view_a_specific_event(req, res, next, event_id) {
+/**
+ * Fetches details of a specific event by its ID.
+ */
+module.exports.view_a_specific_event = function view_a_specific_event(_, res, /* eslint-disable no-unused-vars */ _next, event_id) {
   Event.view_a_specific_event(event_id)
     .then(function (response) {
       utils.writeJson(res, response, 200);
@@ -13,7 +16,10 @@ module.exports.view_a_specific_event = function view_a_specific_event(req, res, 
     });
 };
 
-module.exports.view_events = function view_events (req, res, next) {
+/**
+ * Fetches a list of all available events.
+ */
+module.exports.view_events = function view_events (_, res, /* eslint-disable no-unused-vars */ _next) {
   Event.view_events()
     .then(function (response) {
       utils.writeJson(res, response);
