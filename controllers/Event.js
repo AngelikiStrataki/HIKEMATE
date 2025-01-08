6ef0e19e0fa05bbdb602d46599bd377ac7f80d5d
Angeliki Strataki <153185530+AngelikiStrataki@users.noreply.github.com>
2025-01-08 15:58:51 +0200

    Update Event.js

--- controllers/Event.js
+++ controllers/Event.js
@@ -3,28 +3,24 @@
 var utils = require('../utils/writer.js');
 var Event = require('../service/EventService');
 
-module.exports.creatEvent = function creatEvent (req, res, next, body) {
-  Event.creatEvent(body)
+/**
+ * Fetches details of a specific event by its ID.
+ */
+module.exports.view_a_specific_event = function view_a_specific_event(_, res, /* eslint-disable no-unused-vars */ _next, event_id) {
+  Event.view_a_specific_event(event_id)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
+    .catch(function (error) {
+      utils.writeJson(res, { message: error.message }, 404);
     });
 };
 
-module.exports.view a specific event = function view a specific event (req, res, next, event_id) {
-  Event.view a specific event(event_id)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.view events = function view events (req, res, next) {
-  Event.view events()
+/**
+ * Fetches a list of all available events.
+ */
+module.exports.view_events = function view_events (_, res, /* eslint-disable no-unused-vars */ _next) {
+  Event.view_events()
     .then(function (response) {
       utils.writeJson(res, response);
     })
