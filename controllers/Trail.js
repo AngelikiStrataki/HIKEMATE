e8c807af1d9dea2784557b050b20e6223d39a256
Angeliki Strataki <153185530+AngelikiStrataki@users.noreply.github.com>
2025-01-08 16:00:19 +0200

    Update Trail.js

--- controllers/Trail.js
+++ controllers/Trail.js
@@ -3,18 +3,20 @@
 var utils = require('../utils/writer.js');
 var Trail = require('../service/TrailService');
 
-
-module.exports.deleteTrail = function deleteTrail (req, res, next, trail_id) {
+/**
+ * Deletes a specific trail by its ID.
+ */
+module.exports.deleteTrail = function deleteTrail(_, res, __, trail_id) {
   Trail.deleteTrail(trail_id)
     .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
       utils.writeJson(res, response);
     });
 };
 
-module.exports.rateTrail = function rateTrail (req, res, next, body, trail_id) {
+/**
+ * Rates a specific trail based on user input.
+ */
+module.exports.rateTrail = function rateTrail (_, res, __, body, trail_id) {
   Trail.rateTrail(body, trail_id)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -24,18 +26,27 @@ module.exports.rateTrail = function rateTrail (req, res, next, body, trail_id) {
     });
 };
 
-
-module.exports.uploadPhotos = function uploadPhotos (req, res, next, trail_id) {
-  Trail.uploadPhotos(trail_id)
-    .then(function (response) {
-      utils.writeJson(res, response);
+/**
+ * Uploads photos for a specific trail.
+ */
+module.exports.uploadPhotos = function uploadPhotos(_, res, __, body, trail_id) {
+  Trail.uploadPhotos(trail_id, body)
+    .then((response) => {
+      utils.writeJson(res, response, 201); // Successful response
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
+    .catch((error) => {
+      console.error("Error in uploadPhotos:", error); // Print the error
+      console.error("Error stack trace:", error.stack); // Print the stack trace
+      const statusCode = error.statusCode || 500;
+      const message = error.message || "Internal Server Error";
+      utils.writeJson(res, { message }, statusCode); // Return error response
     });
 };
 
-module.exports.useForum = function useForum (req, res, next, trail_id) {
+/**
+ * Accesses the forum associated with a specific trail.
+ */
+module.exports.useForum = function useForum (_, res, __, trail_id) {
   Trail.useForum(trail_id)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -45,7 +56,10 @@ module.exports.useForum = function useForum (req, res, next, trail_id) {
     });
 };
 
-module.exports.view_a_specific_trail = function view_a_specific_trail(req, res, next, trail_id) {
+/**
+ * Retrieves details of a specific trail by its ID.
+ */
+module.exports.view_a_specific_trail = function view_a_specific_trail(_, res, __, trail_id) {
   Trail.view_a_specific_trail(trail_id)
     .then(function (response) {
       utils.writeJson(res, response, 200);
@@ -55,9 +69,11 @@ module.exports.view_a_specific_trail = function view_a_specific_trail(req, res, 
     });
 };
 
-
-module.exports.view trails = function view trails (req, res, next) {
-  Trail.view trails()
+/**
+ * Retrieves a list of all available trails.
+ */
+module.exports.view_trails = function view_trails (_, res, __) {
+  Trail.view_trails()
     .then(function (response) {
       utils.writeJson(res, response);
     })
@@ -66,17 +82,28 @@ module.exports.view trails = function view trails (req, res, next) {
     });
 };
 
-module.exports.viewPhotos = function viewPhotos (req, res, next, trail_id) {
+/**
+ * Retrieves photos associated with a specific trail.
+ */
+module.exports.viewPhotos = function viewPhotos(_, res, __, trail_id) {
   Trail.viewPhotos(trail_id)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200); // Successful response
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
+    .catch(function (error) {
+      // Ensure the error contains a statusCode and message
+      const statusCode = error.statusCode || 500; // Default status 500 if not present
+      const message = error.message || 'Internal Server Error';
+
+      // Write the error response
+      utils.writeJson(res, { message }, statusCode);
     });
 };
 
-module.exports.viewTrailRating = function viewTrailRating (req, res, next, trail_id) {
+/**
+ * Retrieves the rating of a specific trail.
+ */
+module.exports.viewTrailRating = function viewTrailRating (_, res, __, trail_id) {
   Trail.viewTrailRating(trail_id)
     .then(function (response) {
       utils.writeJson(res, response);
