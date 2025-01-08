5f0a1cd5a1970bdcb2a0447f5c53cf3eea8beabe
eftychiaak <eutychiaka6@gmail.com>
2025-01-08 15:27:07 +0200

    Update Searchfilters.js

--- controllers/Searchfilters.js
+++ controllers/Searchfilters.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var utils = require('../utils/writer.js');
 var Searchfilters = require('../service/SearchfiltersService');
@@ -20,5 +20,35 @@ module.exports.suggest trails = function suggest trails (req, res, next) {
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+    });
+};*/
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Searchfilters = require('../service/SearchfiltersService');
+
+/**
+ * Handles the submission of search filters.
+ * Validates the input and processes the search filters if valid.
+ */
+module.exports.enterSearchfilters = function enterSearchfilters(_ , res, /* eslint-disable no-unused-vars */ _next, body) {
+
+  if (!body || Object.keys(body).length === 0) {
+    utils.writeJson(res, { message: 'Invalid or missing filters' }, 400);
+    return;
+  }
+
+  Searchfilters.enterSearchfilters(body)
+    .then(function (response) {
+      utils.writeJson(res, response, 201);
+    })
+    .catch(function (error) {
+      console.error('Searchfilters.enterSearchfilters error:', error);
+
+      const statusCode = error.message === 'No filters provided.' || error.message === 'No trails found matching the filters.'
+        ? 400
+        : 500;
+
+      utils.writeJson(res, { message: error.message }, statusCode);
     });
 };
